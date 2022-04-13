cf6d65262546c200ca95137bdd37d68fc95597b2
Sergey-Kosichkin <68821779+Sergey-Kosichkin@users.noreply.github.com>
2022-04-13 22:46:58 +0300

    Created traffic light and button Added logic for button click and switch traffic light views

--- RepeatTrafficLight/ViewController.swift
+++ RepeatTrafficLight/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSignalView: UIView!
+    @IBOutlet var yellowSignalView: UIView!
+    @IBOutlet var greenSignalView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    private let offSignal: CGFloat = 0.3
+    private let onSignal: CGFloat = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSignalView.alpha = offSignal
+        yellowSignalView.alpha = offSignal
+        greenSignalView.alpha = offSignal
+        
+        startButton.layer.cornerRadius = 10
     }
 
-
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        redSignalView.layer.cornerRadius =
+            redSignalView.bounds.size.width / 2
+        
+        yellowSignalView.layer.cornerRadius =
+            yellowSignalView.bounds.size.width / 2
+        
+        greenSignalView.layer.cornerRadius =
+            greenSignalView.bounds.size.width / 2
+    }
+    
+    @IBAction func startButtonPressed() {
+        if greenSignalView.alpha == onSignal {
+            greenSignalView.alpha = offSignal
+            redSignalView.alpha = onSignal
+        } else if yellowSignalView.alpha == onSignal {
+            yellowSignalView.alpha = offSignal
+            greenSignalView.alpha = onSignal
+        } else if redSignalView.alpha == onSignal {
+            redSignalView.alpha = offSignal
+            yellowSignalView.alpha = onSignal
+        } else {
+            redSignalView.alpha = onSignal
+            startButton.setTitle("NEXT", for: .normal)
+        }
+    }
+    
 }
 
